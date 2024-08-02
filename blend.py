e1945d3f85a736f75a05aaf1f57ad84fe77bf227
Jeremy Gao <145064583+WenSimEHRP@users.noreply.github.com>
2024-08-01 21:31:52 -0700

    Change: split function info class

--- blend.py
+++ blend.py
@@ -10,19 +10,19 @@ class Print:
     @staticmethod
     def error(*message, sep=" "):
         sys.stderr.write(
-            f"{Print.colours['red']}Error: {sep.join(message)}{Print.colours['reset']}\n"
+            f"{Print.colours['red']}Error: {sep.join(map(str, message))}{Print.colours['reset']}\n"
         )
         sys.exit(1)
 
     @staticmethod
     def warn(*message, sep=" "):
         sys.stderr.write(
-            f"{Print.colours['yellow']}Warning: {sep.join(message)}{Print.colours['reset']}\n"
+            f"{Print.colours['yellow']}Warning: {sep.join(map(str, message))}{Print.colours['reset']}\n"
         )
 
     @staticmethod
     def info(*message, sep=" "):
-        sys.stdout.write(f"{sep.join(message)}\n")
+        sys.stdout.write(f"{sep.join(map(str, message))}\n")
 
     @staticmethod
     # does not print anything but colours the message
@@ -36,6 +36,7 @@ class ProcessImage:
         self._image = None
         self._spritemap = None
         self._used_colours = None
+        self._size = None
 
     def _load_image(self):
         try:
@@ -73,11 +74,19 @@ class ProcessImage:
             self._used_colours = self.get_used_colours(self.spritemap)
         return self._used_colours
 
+    @property
+    def size(self):
+        if self._size is None:
+            self._size = self.image.size
+        return self._size
+
 
 class ProcessedImage:
     def __init__(self, spritemap):
         self.spritemap = spritemap
+        self.image_path = "Internal image"
         self._used_colours = None
+        self._size = None
 
     @property
     def used_colours(self) -> set:
@@ -85,30 +94,77 @@ class ProcessedImage:
             self._used_colours = ProcessImage.get_used_colours(self.spritemap)
         return self._used_colours
 
+    @property
+    def size(self):
+        if self._size is None:
+            self._size = (len(self.spritemap[0]), len(self.spritemap))
+        return self._size
+
 
 class CompareImage:
     def __init__(self, patient1, patient2):
+        if not patient1.size == patient2.size:
+            Print.info(patient1.size, patient2.size)
+            Print.error(
+                f"Images {patient1.image_path} and {patient2.image_path} are not the same size"
+            )
+
         self.patient1 = patient1
         self.patient2 = patient2
+
+        self._rec_info = None
+        self._spritemap = None
+        self._recolour_dict1 = None
+        self._recolour_dict2 = None
+        self._used_colours = None
+        self._estimated_colour_count = None
+
+    @property
+    def rec_info(self):
+        if self._rec_info is None:
+            self._rec_info = self.get_recinfo(self.patient1, self.patient2)
+        return self._rec_info
+
+    @property
+    def spritemap(self):
+        if self._spritemap is None:
+            self._spritemap, self._recolour_dict1, self._recolour_dict2 = self.rec_info
+        return self._spritemap
+
+    @property
+    def recolour_dict1(self):
+        if self._recolour_dict1 is None:
+            self._spritemap, self._recolour_dict_1, self._recolour_dict2 = self.rec_info
+        return self._recolour_dict1
+
+    @property
+    def recolour_dict2(self):
+        if self._recolour_dict2 is None:
+            self._spritemap, self._recolour_dict_1, self._recolour_dict2 = self.rec_info
+        return self._recolour_dict2
+
+    @property
+    def used_colours(self):
+        if self._used_colours is None:
+            self._used_colours = len(ProcessImage.get_used_colours(self.spritemap))
+        return self._used_colours
+
+    @property
+    def estimated_colour_count(self):
+        if self._estimated_colour_count is None:
+            colours1 = len(self.patient1.used_colours)
+            colours2 = len(self.patient2.used_colours)
+            self._estimated_colour_count = (
+                max(colours1, colours2),
+                colours1 * colours2,
+            )
+        return self._estimated_colour_count
 
     @staticmethod
     def get_recinfo(
         image1: Union[ProcessImage, ProcessedImage],
         image2: Union[ProcessImage, ProcessedImage],
     ) -> tuple[tuple, dict, dict]:
-        # FIXME sep the function to proper class
-        # check image size
-        if not image1.image.size == image2.image.size:
-            Print.error(
-                f"Images {image1.image_path} and {image2.image_path} are not the same size"
-            )
-        # evaluate colours
-        Print.info(
-            f"Estimated colour count: "
-            + f"{max(len(image1.used_colours), len(image2.used_colours))}"
-            + "-"
-            + f"{len(image1.used_colours) * len(image2.used_colours)}"
-        )
         # data initialization
         new_spritemap = [
             [0 for _ in range(len(image1.spritemap[0]))]
@@ -156,7 +212,6 @@ class CompareImage:
                     recolour_dict1[new_colour] = colour1
                     recolour_dict2[new_colour] = colour2
 
-        Print.info(f"Colours in final spritemap: {len(common_colours)}")
         return (tuple(new_spritemap), recolour_dict1, recolour_dict2)
 
 
@@ -170,19 +225,32 @@ def gen_recolour_sprite(rec1, rec2):
 
 def process_image(image_paths: list[str]) -> tuple:
     images = [ProcessImage(image_path) for image_path in image_paths]
-    spritemap, rec1, rec2 = CompareImage.get_recinfo(images[0], images[1])
+    new_image = CompareImage(images[0], images[1])
+    Print.info(
+        f"Estimated colours: {new_image.estimated_colour_count[0]}-{new_image.estimated_colour_count[1]}"
+    )
+    Print.info(f"Actual           : {new_image.used_colours}")
+    spritemap = new_image.spritemap
+    rec1 = new_image.recolour_dict1
+    rec2 = new_image.recolour_dict2
 
     recolour_sprites = {image_paths[0]: rec1.copy(), image_paths[1]: rec2.copy()}
     processed = ProcessedImage(spritemap)
 
     for i in range(2, len(images)):
-        spritemap, base_rec, new_rec = CompareImage.get_recinfo(processed, images[i])
-        processed.spritemap = spritemap
+        new_image = CompareImage(processed, images[i])
+        Print.info(
+            f"Estimated colours: {new_image.estimated_colour_count[0]}-{new_image.estimated_colour_count[1]}"
+        )
+        Print.info(f"Actual           : {new_image.used_colours}")
+        processed.spritemap = new_image.spritemap
 
         for key, recolour_sprite in recolour_sprites.items():
-            recolour_sprites[key] = gen_recolour_sprite(recolour_sprite, base_rec)
+            recolour_sprites[key] = gen_recolour_sprite(
+                recolour_sprite, new_image.recolour_dict1
+            )
 
-        recolour_sprites[image_paths[i]] = new_rec
+        recolour_sprites[image_paths[i]] = new_image.recolour_dict2
 
     return (processed.spritemap, images[0].image.getpalette(), recolour_sprites)
 
@@ -196,10 +264,10 @@ def write_image(filename: str, data: tuple, palette: Image.Palette) -> None:
 
 def format_recolour_data(recolour_data: dict[dict]) -> dict:
     f = {}
-    for ind, rec in recolour_data.items():
+    for name, rec in recolour_data.items():
         s = ""
         s += "recolour_sprite {"
-        s += f"\n    // {ind}"
+        s += f"\n    // {name}"
         counter = 0
         for key, value in rec.items():
             if key == value:
@@ -209,8 +277,8 @@ def format_recolour_data(recolour_data: dict[dict]) -> dict:
             s += f"0x{key:02x}:0x{value:02x};"
             counter += 1
         s += "\n}\n"
-        Print.info(f"Used colours: {counter}")
-        f[ind] = s
+        Print.info(f"Used colours: {counter} ({name})")
+        f[name] = s
     return f
 
 
